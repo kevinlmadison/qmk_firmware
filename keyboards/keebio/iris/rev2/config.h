f2f887eb5e4e370f156ec489241c5678853abe0b
kevinlmadison <coolklm121@gmail.com>
2024-04-21 09:39:05 -0400

    my qmk setups

--- keyboards/keebio/iris/rev2/config.h
+++ keyboards/keebio/iris/rev2/config.h
@@ -31,8 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
-#define RGBLED_NUM 12    // Number of LEDs
-#define RGBLED_SPLIT { 6, 6 }
+#define RGBLED_NUM 24    // Number of LEDs
+#define RGBLED_SPLIT { 12, 12 }
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
--- keyboards/keebio/iris/rev2/rules.mk
+++ keyboards/keebio/iris/rev2/rules.mk
@@ -1,10 +1,11 @@
 # Build Options
 #   change yes to no to disable
 #
+LINK_TIME_OPTIMIZATION_ENABLE = yes
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
